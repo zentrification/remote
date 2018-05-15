af46396caf2800c1dee8b376a46b847b031ca827
Chris Dahms <zentrification@gmail.com>
2018-05-15 19:07:29 -0400

    click to copy login/password

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // copy username/password when clicked
+  for (var element of document.getElementsByClassName('tag')) {
+    element.onclick = function(e) {
+      e.preventDefault();
+      document.getElementById('clipboard').value = e.srcElement.innerText;
+      document.getElementById('clipboard').select();
+      document.execCommand('copy');
+    };
+  }
+
+});
